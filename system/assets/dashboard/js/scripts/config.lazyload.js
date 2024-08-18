d6e4296ae5da9dd4110cb7e12ebfe7a3db32e79a
aalaswad275 <aalaswad275@gmail.com>
2024-08-18 12:01:01 +0200

    add

---
+++ system/assets/dashboard/js/scripts/config.lazyload.js
@@ -0,0 +1,69 @@
+// lazyload config
+var MODULE_CONFIG = {
+    easyPieChart: [public_folder_path + '/assets/dashboard/js/jquery.easy-pie-chart/dist/jquery.easypiechart.fill.js'],
+    sparkline: [public_folder_path + '/assets/dashboard/js/jquery.sparkline/dist/jquery.sparkline.retina.js'],
+    plot: [public_folder_path + '/assets/dashboard/js/flot/jquery.flot.js',
+        public_folder_path + '/assets/dashboard/js/flot/jquery.flot.resize.js',
+        public_folder_path + '/assets/dashboard/js/flot/jquery.flot.pie.js',
+        public_folder_path + '/assets/dashboard/js/flot.tooltip/js/jquery.flot.tooltip.min.js',
+        public_folder_path + '/assets/dashboard/js/flot-spline/js/jquery.flot.spline.min.js',
+        public_folder_path + '/assets/dashboard/js/flot.orderbars/js/jquery.flot.orderBars.js'],
+    vectorMap: [public_folder_path + '/assets/dashboard/js/bower-jvectormap/jquery-jvectormap-1.2.2.min.js',
+        public_folder_path + '/assets/dashboard/js/bower-jvectormap/jquery-jvectormap.css',
+        public_folder_path + '/assets/dashboard/js/bower-jvectormap/jquery-jvectormap-world-mill-en.js',
+        public_folder_path + '/assets/dashboard/js/bower-jvectormap/jquery-jvectormap-us-aea-en.js'],
+    dataTable: [
+        public_folder_path + '/assets/dashboard/js/datatables/media/js/jquery.dataTables.min.js',
+        public_folder_path + '/assets/dashboard/js/plugins/integration/bootstrap/3/dataTables.bootstrap.js',
+        public_folder_path + '/assets/dashboard/js/plugins/integration/bootstrap/3/dataTables.bootstrap.css'],
+    footable: [
+        public_folder_path + '/assets/dashboard/js/footable/dist/footable.all.min.js',
+        public_folder_path + '/assets/dashboard/js/footable/css/footable.core.css'
+    ],
+    sortable: [
+        public_folder_path + '/assets/dashboard/js/html.sortable/dist/html.sortable.min.js'
+    ],
+    nestable: [
+        public_folder_path + '/assets/dashboard/js/nestable/jquery.nestable.css',
+        public_folder_path + '/assets/dashboard/js/nestable/jquery.nestable.js'
+    ],
+    summernote: [
+        public_folder_path + '/assets/dashboard/js/summernote/dist/summernote.css',
+        public_folder_path + '/assets/dashboard/js/summernote/dist/summernote.js'
+    ],
+    parsley: [
+        public_folder_path + '/assets/dashboard/js/parsleyjs/dist/parsley.css',
+        public_folder_path + '/assets/dashboard/js/parsleyjs/dist/parsley.min.js'
+    ],
+    select2: [
+        public_folder_path + '/assets/dashboard/js/select2/dist/css/select2.min.css',
+        public_folder_path + '/assets/dashboard/js/select2-bootstrap-theme/dist/select2-bootstrap.min.css',
+        public_folder_path + '/assets/dashboard/js/select2-bootstrap-theme/dist/select2-bootstrap.4.css',
+        public_folder_path + '/assets/dashboard/js/select2/dist/js/select2.min.js'
+    ],
+    datetimepicker: [
+        public_folder_path + '/assets/dashboard/js/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css',
+        public_folder_path + '/assets/dashboard/js/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.dark.css',
+        public_folder_path + '/assets/dashboard/js/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js'
+    ],
+    chart: [
+        public_folder_path + '/assets/dashboard/js/echarts/build/dist/echarts-all.js',
+        public_folder_path + '/assets/dashboard/js/echarts/build/dist/theme.js',
+        public_folder_path + '/assets/dashboard/js/echarts/build/dist/jquery.echarts.js'
+    ],
+    bootstrapWizard: [
+        public_folder_path + '/assets/dashboard/js/twitter-bootstrap-wizard/jquery.bootstrap.wizard.min.js'
+    ],
+    fullCalendar: [
+        public_folder_path + '/assets/dashboard/js/moment/moment.js',
+        public_folder_path + '/assets/dashboard/js/fullcalendar/dist/fullcalendar.min.js',
+        public_folder_path + '/assets/dashboard/js/fullcalendar/dist/lang/' + public_lang + '.js',
+        public_folder_path + '/assets/dashboard/js/fullcalendar/dist/fullcalendar.css',
+        public_folder_path + '/assets/dashboard/js/fullcalendar/dist/fullcalendar.theme.css',
+        public_folder_path + '/assets/dashboard/js/scripts/plugins/calendar.js'
+    ],
+    dropzone: [
+        public_folder_path + '/assets/dashboard/js/dropzone/dist/min/dropzone.min.js',
+        public_folder_path + '/assets/dashboard/js/dropzone/dist/min/dropzone.min.css'
+    ]
+};
